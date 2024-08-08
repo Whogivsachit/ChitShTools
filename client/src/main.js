94de891c25c89939621518f8e774cd9cd29276ed
Whogivsachit <evanm6257@gmail.com>
2024-08-07 22:36:14 -0700

    Prod

--- client/src/main.js
+++ client/src/main.js
@@ -36,7 +36,7 @@ app.directive('click-outside', clickOutside)
 // Global Variables
 app.config.globalProperties.$apiUrl = '/api'
 app.config.globalProperties.$appName = 'ChitShTools'
-app.config.globalProperties.$appUrl = 'http://localhost:5173'
+app.config.globalProperties.$appUrl = 'http://localhost:80'
 app.config.globalProperties.$socketUrl = 'http://localhost:3000'
 
 app.mount('#app')
